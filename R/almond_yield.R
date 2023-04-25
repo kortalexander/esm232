6ff1dce74e261be98fb575741fa8b302fd6a9f9e
kortalexander <kort.alexander1@gmail.com>
2023-04-25 10:31:03 -0700

    added units to documentation and put parameters in function definition

--- R/almond_yield.R
+++ R/almond_yield.R
@@ -1,9 +1,9 @@
 #' Calculate the maximum, minimum, and mean almond yield anomaly for a given time series.
 #' 
-#' @param clim_data Data frame of daily climate data for time period of interest. Columns: day, month, year, wy, tmax_c, tmin_c, precip.
-#' @returns List of max, min, and mean almond yield anomalies.
+#' @param clim_data Data frame of daily climate data for time period of interest. Columns: day, month, year, wy, tmax (degrees C), tmin (degrees C), precip (mm).
+#' @returns List of max, min, and mean almond yield anomalies in tons/acre.
 
-almond_yield <- function(clim_data) {
+almond_yield <- function(clim_data, Tmincoeff1 = -0.015, Tmincoeff2 = -0.0046, Pcoeff1 = -0.07, Pcoeff2 = 0.0043, intercep = 0.28) {
   
   # process climate data into monthly averages
   clim_monthly <- clim_data %>% 
@@ -25,11 +25,7 @@ almond_yield <- function(clim_data) {
     }
     
     # extract needed values by referencing the row and column and input into formula
-    yield_anomaly_i <- -0.015 * (year_i_data[year_i_data$month==2, ][[4]]) -
-      0.0046 * (year_i_data[year_i_data$month==2, ][[4]])^2 -
-      0.07 * (year_i_data[year_i_data$month==1, ][[5]]) +
-      0.0043 * (year_i_data[year_i_data$month==1, ][[5]])^2 +
-      0.28
+    yield_anomaly_i <- Tmincoeff1 * (year_i_data[year_i_data$month==2, ][[4]]) + Tmincoeff2 * (year_i_data[year_i_data$month==2, ][[4]])^2 + Pcoeff1 * (year_i_data[year_i_data$month==1, ][[5]]) + Pcoeff2 * (year_i_data[year_i_data$month==1, ][[5]])^2 + intercep
     
     # add to list of anomalies
     yield_anomalies <- yield_anomalies %>% append(yield_anomaly_i)
